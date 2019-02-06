8328ad87a7d5d8ae9c871939dcc2e1491fd0b7af
maeva coinin <you@example.com>
2019-02-06 22:14:07 +0100

    ok

--- test/test.js
+++ test/test.js
@@ -2,7 +2,30 @@ const assert = require("assert");
 
 let classifier = require("./training.js");
 
-before(function() {});
+before(function () { });
+
+/**
+ * SUJET + VERBE d'ÉTAT + ADVERBE + ADJECTIF
+ * SUJET + VERBE  + COMPLEMENT d'OBJET
+ * 
+ * [sujet] que... corps du SMS ou de l'email
+ * 
+ */
+
+capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+extractTxt = (str) => {
+  let regex = /(que|que l'|qui|qu'on|qu').*$/ig
+  let matchObj = str.match(regex);
+
+  if (matchObj && matchObj.length > 0) {
+    matchObj[0] = matchObj[0].replace(/(que|que l'|qui|qu')/, "");
+  }
+
+  return (matchObj && matchObj.length > 0) ? capitalizeFirstLetter(matchObj[0]).trim() : null;
+
+
+}
 
 describe("Test on Bayesienne Training", () => {
   describe("Just test some intent", () => {
@@ -69,5 +92,43 @@ describe("Test on Bayesienne Training", () => {
       );
       assert.equal("sms", cat);
     });
+
+    it("Intent SMS", () => {
+      let cat = classifier.categorize(
+        "Envois un SMS à Simon"
+      );
+      assert.equal("sms", cat);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Envois un message à Simon pour lui dire qu'on y sera là demain.")
+      assert.equal("On y sera là demain.", res);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Envois un SMS à Manu que l'on sera 2 finalement.")
+      assert.equal("l'on sera 2 finalement.", res);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Envois un message à Simon qu'il n'y a plus de place en amphi")
+      assert.equal("Il n'y a plus de place en amphi", res);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Envois un message à Juju qu'on est 5 finalement")
+      assert.equal("On est 5 finalement", res);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Tu peux dire à Manu qu'il serait temps de s'y mettre!")
+      assert.equal("Il serait temps de s'y mettre!", res);
+    });
+
+    it("COD, COI, Complément", () => {
+      let res = extractTxt("Tu peux dire à Manu qu'il serait temps de s'y mettre!")
+      assert.equal("Il serait temps de s'y mettre!", res);
+    });
+
   });
 });
