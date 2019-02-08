0fc6c820be2d1de34a5139ed6a6bbe3cdad92e5c
maeva coinin <you@example.com>
2019-02-08 08:59:54 +0100

    ok

--- gpio.js
+++ gpio.js
@@ -8,7 +8,7 @@ gpiop.setup(8, gpio.DIR_OUT)
             .then(() => {
                 gpiop.write(7, true)
                 gpiop.write(8, true)
-                setTimeout(() => gpiop.write(7, false), 3000)
+                // setTimeout(() => gpiop.write(7, false), 3000)
             })
             .catch((err) => {
                 console.log('Error: ', err.toString())
