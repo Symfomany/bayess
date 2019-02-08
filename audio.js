60be3940879de665cf0a73259d196b0190d2417f
Your Name <you@example.com>
2019-02-08 16:57:19 +0100

    ok

---
+++ audio.js
@@ -0,0 +1,34 @@
+var load = require("audio-loader");
+// const player = require("play-sound")((opts = {}));
+// const stream = require("stream");
+// const Speaker = require("speaker");
+var play = require("audio-play");
+
+var ac = require("audio-context")();
+
+// let speaker = new Speaker();
+
+// function playAudioFromBuffer(fileContents) {
+//   let bufferStream = new stream.PassThrough();
+//   bufferStream.end(fileContents);
+//   bufferStream.pipe(speaker);
+// }
+function err(error) {
+  console.error("Error:", error);
+}
+
+load("https://sample-videos.com/audio/mp3/crowd-cheering.mp3")
+  .then(buffer => {
+    var sample = player(ac, buffer);
+    sample.start();
+  })
+  .catch(err);
+
+// load one file
+// load("https://sample-videos.com/audio/mp3/crowd-cheering.mp3").then(buffer => {
+//   //console.log(buffer); // => <AudioBuffer>
+//   player.play(buffer, err => {
+//     if (err) console.log(err);
+//     playAudioFromBuffer(buffer);
+//   });
+// });
