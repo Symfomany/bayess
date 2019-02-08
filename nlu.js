5db44881e36d1c077619fdf50e5a351504e2a3a3
maeva coinin <you@example.com>
2019-02-08 06:17:19 +0100

    ok

--- nlu.js
+++ nlu.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const client = new speech.SpeechClient();
 
 // The name of the audio file to transcribe
-const fileName = './myvoice.wav';
+const fileName = './output.raw';
 
 // Reads a local audio file and converts it to base64
 const file = fs.readFileSync(fileName);
