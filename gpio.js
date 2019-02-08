7d8523f77a970334bce46ae0a67bb669de803ba1
maeva coinin <you@example.com>
2019-02-08 08:56:37 +0100

    ok

--- gpio.js
+++ gpio.js
@@ -1,12 +1,20 @@
 var gpio = require('rpi-gpio')
 var gpiop = gpio.promise;
+gpio.setup(7, gpio.DIR_OUT);
+gpiop.setup(8, gpio.DIR_OUT);
 
-gpiop.setup(7, gpio.DIR_OUT)
-    .then(() => {
-        gpiop.write(7, false)
-        gpiop.write(8, true)
-        setTimeout(() => { gpiop.write(7, true); }, 1000)
-    })
-    .catch((err) => {
-        console.log('Error: ', err.toString())
-    })
+gpiop.write(7, true)
+gpiop.write(8, true)
+
+gpio.on('export', function (channel) {
+    console.log('Channel set: ' + channel);
+});
+
+
+    //     gpiop.write(7, false)
+    //     gpiop.write(8, true)
+    //     setTimeout(() => gpiop.write(7, true), 1000)
+    // })
+    // .catch((err) => {
+    //     console.log('Error: ', err.toString())
+    // })
