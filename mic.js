141a0c74502a823e5f717a577b83fce5e8a0ecff
maeva coinin <you@example.com>
2019-02-08 06:19:15 +0100

    ok

--- mic.js
+++ mic.js
@@ -1,6 +1,33 @@
 var FileWriter = require('wav').FileWriter;
 var mic = require('mic'); // requires arecord or sox, see https://www.npmjs.com/package/mic
 var fs = require('fs');
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+// The name of the audio file to transcribe
+const fileName = './output.raw';
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+    content: audioBytes,
+};
+const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'fr-FR',
+};
+const request = {
+    audio: audio,
+    config: config,
+};
 
 var micInstance = mic({
     rate: '16000',
@@ -26,7 +53,20 @@ micInputStream.on('startComplete', function () {
     console.log("Got SIGNAL startComplete");
     setTimeout(function () {
         micInstance.pause();
-    }, 5000);
+        // Detects speech in the audio file
+        client.recognize(request)
+            .then(data => {
+                const response = data[0];
+                const transcription = response.results
+                    .map(result => result.alternatives[0].transcript)
+                    .join('\n');
+                console.log(`Transcription: ${transcription}`);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+
+    }, 3000);
 });
 
 micInputStream.on('stopComplete', function () {
