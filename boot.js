c6cb5da45dbbf727da1a3a714a565f648e8d1328
Your Name <you@example.com>
2019-02-10 14:03:15 +0100

    commit

--- boot.js
+++ boot.js
@@ -4,14 +4,13 @@ const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
 // parser.on("data", line => console.log(`> ${line}`));
 
 console.log("Send 1 to Arduino");
-port.write("a");
 
-// const blinkLED = () => {
-//   port.write("a");
-// };
-// const blinkInterval = setInterval(blinkLED, ); //run the blinkLED function every 250ms
-// const endBlink = () => {
-//   clearInterval(blinkInterval); // Stop blink intervals
-// };
+const blinkLED = () => {
+  port.write("a");
+};
+const blinkInterval = setInterval(blinkLED, 2000); //run the blinkLED function every 250ms
+const endBlink = () => {
+  clearInterval(blinkInterval); // Stop blink intervals
+};
 
-// setTimeout(endBlink, 20000); //stop blinking after 5 seconds
+setTimeout(endBlink, 20000); //stop blinking after 5 seconds
