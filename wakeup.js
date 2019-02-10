43f2e9bccbf5993157b5f5855f47195b9390f4e8
Your Name <you@example.com>
2019-02-10 09:39:54 +0100

    ok

--- wakeup.js
+++ wakeup.js
@@ -8,15 +8,39 @@ player.play("./wakeup.wav", err => {
 
 console.log("Playing music");
 
-gpiop.setup(7, gpio.DIR_OUT).then(() => {
-  gpiop.write(7, true);
-  let intervalFunc = () => gpiop.write(7, true);
-  let intervalFuncTwo = () => gpiop.write(7, false);
+// var gpio = require("gpio");
+// var gpio7, intervalTimer;
+var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
-  setInterval(intervalFunc, 1500);
-  setInterval(intervalFuncTwo, 2000);
+function blinkLED() {
+  //function to start blinking
+  if (LED.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
 
-  console.log("Go GPIO");
+function endBlink() {
+  //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
 
-  setTimeout(() => gpiop.write(7, false), 7000);
-});
+setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+// gpiop.setup(7, gpio.DIR_OUT).then(() => {
+//   gpiop.write(7, true);
+//   let intervalFunc = () => gpiop.write(7, true);
+//   let intervalFuncTwo = () => gpiop.write(7, false);
+
+//   setInterval(intervalFunc, 1500);
+//   setInterval(intervalFuncTwo, 2000);
+
+//   console.log("Go GPIO");
+
+//   setTimeout(() => gpiop.write(7, false), 7000);
+// });
