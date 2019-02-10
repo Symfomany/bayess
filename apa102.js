e62b26f41c82c3d5889e4d3a3d14f60fb07a44d2
Your Name <you@example.com>
2019-02-10 15:29:17 +0100

    commit

--- apa102.js
+++ apa102.js
@@ -1,6 +1,6 @@
 var hooloovoo = require("hooloovoo");
 hooloovoo.setup(12); // The number of LEDs
 
-hooloovoo.set_pixel_hex(0, FF0000); // set LED1 to red
+hooloovoo.set_pixel_BGR(0, 0, 0, 255); // set LED1 to red
 
 // hooloovoo.set_pixel_RGB(0, 255, 0, 0); // set LED1 to red
