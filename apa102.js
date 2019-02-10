ad14bd961dac9e1be606474c00d4def81dc21145
Your Name <you@example.com>
2019-02-10 15:30:19 +0100

    commit

--- apa102.js
+++ apa102.js
@@ -3,5 +3,5 @@ hooloovoo.setup(12); // The number of LEDs
 
 hooloovoo.set_pixel_BGR(0, 0, 0, 255); // set LED1 to red
 
-setTimeout(() => 12000);
+setTimeout(() => true, 12000);
 // hooloovoo.set_pixel_RGB(0, 255, 0, 0); // set LED1 to red
