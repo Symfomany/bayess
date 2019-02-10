7b4752780c4c6a5153f0d7216906631d604c3ca0
Your Name <you@example.com>
2019-02-10 09:17:50 +0100

    ok

--- wakeup.js
+++ wakeup.js
@@ -6,7 +6,11 @@ player.play("./wakeup.wav", err => {
   if (err) throw err;
 });
 
+console.log("Playing music");
+
 gpiop.setup(7, gpio.DIR_OUT).then(() => {
+  console.log("Go GPIO");
+
   gpiop.write(7, true);
 
   setTimeout(() => gpiop.write(7, false), 5000);
