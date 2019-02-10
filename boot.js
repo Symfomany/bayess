5ce4cacc2d08ee726862a38386974b2b762d6141
Your Name <you@example.com>
2019-02-10 11:44:33 +0100

    commit

--- boot.js
+++ boot.js
@@ -2,6 +2,16 @@ const SerialPort = require("serialport");
 const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
 // port.pipe(parser);
 // parser.on("data", line => console.log(`> ${line}`));
+port.write("1", err => {
+  if (err) {
+    return console.log("Error on write: ", err.message);
+  }
+  console.log("message written");
+});
 
-port.write("1");
+// Open errors will be emitted as an error event
+port.on("error", err => {
+  console.log("Error: ", err.message);
+});
+
 console.log("Send 1 to Arduino");
