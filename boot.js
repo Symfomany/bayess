ee4ee1de3edda968b21c74a551517c6a8a5f344d
Your Name <you@example.com>
2019-02-10 11:39:43 +0100

    commit

--- boot.js
+++ boot.js
@@ -2,4 +2,6 @@ const SerialPort = require("serialport");
 const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
 // port.pipe(parser);
 // parser.on("data", line => console.log(`> ${line}`));
+
 port.write("1");
+console.log("Send 1 to Arduino");
