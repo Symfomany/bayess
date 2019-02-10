8aa44f24de68a0e572e43a9a89e40c20761acce3
Your Name <you@example.com>
2019-02-10 11:30:50 +0100

    commit

--- boot.js
+++ boot.js
@@ -1,5 +1,5 @@
 const SerialPort = require("serialport");
 const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
-port.pipe(parser);
+// port.pipe(parser);
 // parser.on("data", line => console.log(`> ${line}`));
 port.write(1);
