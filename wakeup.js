8fd2f089338a6fa24eb609eb6a9ed50a561fc0ba
Your Name <you@example.com>
2019-02-10 14:12:49 +0100

    commit

--- wakeup.js
+++ wakeup.js
@@ -8,32 +8,38 @@ player.play("./wakeup.wav", err => {
 
 console.log("Playing music");
 
-// var gpio = require("gpio");
-// var gpio7, intervalTimer;
-var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
-const LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+const SerialPort = require("serialport");
+const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
 
-let dec = 250;
+setTimeout(() => port.write("a"), 10000); //stop blinking after 5 seconds
+setTimeout(() => true, 20000); //stop blinking after 5 seconds
 
-setInterval(() => (dec -= 250), 100);
+// // var gpio = require("gpio");
+// // var gpio7, intervalTimer;
+// var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+// const LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
 
-const blinkLED = () => {
-  //function to start blinking
-  if (LED.readSync() === 0) {
-    //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-};
+// let dec = 250;
 
-const blinkInterval = setInterval(blinkLED, dec); //run the blinkLED function every 250ms
+// setInterval(() => (dec -= 250), 100);
 
-let endBlink = () => {
-  //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
-};
+// const blinkLED = () => {
+//   //function to start blinking
+//   if (LED.readSync() === 0) {
+//     //check the pin state, if the state is 0 (or off)
+//     LED.writeSync(1); //set pin state to 1 (turn LED on)
+//   } else {
+//     LED.writeSync(0); //set pin state to 0 (turn LED off)
+//   }
+// };
 
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+// const blinkInterval = setInterval(blinkLED, dec); //run the blinkLED function every 250ms
+
+// let endBlink = () => {
+//   //function to stop blinking
+//   clearInterval(blinkInterval); // Stop blink intervals
+//   LED.writeSync(0); // Turn LED off
+//   LED.unexport(); // Unexport GPIO to free resources
+// };
+
+// setTimeout(endBlink, 5000); //stop blinking after 5 seconds
