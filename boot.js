6f1401f638ad51ec515ce5e8037a3b99d6cef3ff
Your Name <you@example.com>
2019-02-10 11:30:35 +0100

    commit

--- boot.js
+++ boot.js
@@ -1,8 +1,5 @@
 const SerialPort = require("serialport");
-const Readline = require("@serialport/parser-readline");
 const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 }); // 256000
-
-const parser = new Readline();
 port.pipe(parser);
 // parser.on("data", line => console.log(`> ${line}`));
 port.write(1);
