92819999d404a784bd829f1292d2223ab746ab82
Your Name <you@example.com>
2019-02-11 08:44:09 +0100

    commit

--- mic.js
+++ mic.js
@@ -183,7 +183,7 @@ micInputStream.on("startComplete", function() {
           micInstance.stop();
           gpiop.write(7, false);
           gpiop.write(8, true);
-        }, 5000);
+        }, 8000);
       })
       .catch(err => {
         console.log("Error: ", err.toString());
